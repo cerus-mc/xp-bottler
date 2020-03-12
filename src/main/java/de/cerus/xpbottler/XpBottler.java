599fc7f9d2428e8280b02873aef7a0f92b454a07
Cerus <zocks1703@web.de>
2020-03-12 17:41:55 +0100

    Fix xp stuff

--- src/main/java/de/cerus/xpbottler/XpBottler.java
+++ src/main/java/de/cerus/xpbottler/XpBottler.java
@@ -50,7 +50,7 @@ public class XpBottler extends CerusPlugin {
                 if(item.getType() != Material.GLASS_BOTTLE) return;
 
                 // Return if player does not have enough xp points
-                if(player.getTotalExperience() < cost) {
+                if(getPlayerExp(player) < cost) {
                     player.sendMessage("§cYou need at least "+cost+" experience points to do this!");
                     return;
                 }
@@ -62,7 +62,7 @@ public class XpBottler extends CerusPlugin {
                 }
 
                 // Update players xp
-                player.setTotalExperience(player.getTotalExperience()-cost);
+                changePlayerExp(player, -cost);
 
                 // Decrement glass bottle item amount
                 if(item.getAmount() > 1) {
@@ -81,7 +81,59 @@ public class XpBottler extends CerusPlugin {
 
     @Override
     public void onPluginDisable() {
+    }
 
+    // Calculate amount of EXP needed to level up
+    private int getExpToLevelUp(int level){
+        if(level <= 15){
+            return 2*level+7;
+        } else if(level <= 30){
+            return 5*level-38;
+        } else {
+            return 9*level-158;
+        }
+    }
+
+    // Calculate total experience up to a level
+    private int getExpAtLevel(int level){
+        if(level <= 16){
+            return (int) (Math.pow(level,2) + 6*level);
+        } else if(level <= 31){
+            return (int) (2.5*Math.pow(level,2) - 40.5*level + 360.0);
+        } else {
+            return (int) (4.5*Math.pow(level,2) - 162.5*level + 2220.0);
+        }
+    }
+
+    // Calculate player's current EXP amount
+    private int getPlayerExp(Player player){
+        int exp = 0;
+        int level = player.getLevel();
+
+        // Get the amount of XP in past levels
+        exp += getExpAtLevel(level);
+
+        // Get amount of XP towards next level
+        exp += Math.round(getExpToLevelUp(level) * player.getExp());
+
+        return exp;
+    }
+
+    // Give or take EXP
+    private int changePlayerExp(Player player, int exp){
+        // Get player's current exp
+        int currentExp = getPlayerExp(player);
+
+        // Reset player's current exp to 0
+        player.setExp(0);
+        player.setLevel(0);
+
+        // Give the player their exp back, with the difference
+        int newExp = currentExp + exp;
+        player.giveExp(newExp);
+
+        // Return the player's new exp amount
+        return newExp;
     }
 
 }
